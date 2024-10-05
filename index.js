6c4d035ef1fdb01fc6a7a4d19fb0bdc0060a0e43
Jamanul <jk.skb.25@gmail.com>
2024-10-05 10:12:28 +0600

    facing erorr

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ app.use(
   })
 );
 app.use(express.json());
-
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dibths0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
