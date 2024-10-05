6f63367029cafdc614d556f00534e881c195e43c
Jamanul <jk.skb.25@gmail.com>
2024-10-05 22:07:00 +0600

    facing erorr

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
   }));
   
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://air-bnb-client-seven.vercel.app');
+    res.header('Access_Control_Allow_Origin', 'https://air-bnb-client-seven.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
